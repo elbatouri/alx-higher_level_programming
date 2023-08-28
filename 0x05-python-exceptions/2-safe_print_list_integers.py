f3736f0f9249d92047c42ee656da029bdfbc5ef3
el batouri rabi <elbatouri@gmail.com>
2023-08-28 13:19:27 -0700

    pushing all tasks

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+ def safe_print_integer(value):
+ number = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            number += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (number)
