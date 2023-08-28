f8011b9509fc712e0b3bd1a5ae4f87fbd7cebf7b
el batouri rabi <elbatouri@gmail.com>
2023-08-28 13:48:00 -0700

    pushing the modified files

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
- try:
+    """Returns the division of a by b."""
+    try:
         division = a / b
     except (TypeError, ZeroDivisionError):
         division = None
