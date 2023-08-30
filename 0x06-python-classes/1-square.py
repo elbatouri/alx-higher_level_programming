2fbd0f5108f3128ae01e0cae869c2ab015b14273
el batouri rabi <elbatouri@gmail.com>
2023-08-29 17:03:35 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a class named Square."""
+
+
+class Square:
+    """a Square"""
+   
+   def__init__(self, size):
+        """initialize a new Square.
+
+        Arguments:
+            size(int):The size of the Square.
+        """
+        
+        self.__size = size
