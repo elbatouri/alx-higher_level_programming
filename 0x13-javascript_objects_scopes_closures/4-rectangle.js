9bd03f59eb29f5c5c713e93e65016f30b40555ab
elbatouri rabi <elbatouri@gmail.com>
2023-11-14 15:51:05 -0800

    correcting the surface declaration in task 5, 6 and 7

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let surface = '';
+      let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
       }
