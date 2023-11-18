be8068664b70bf605ccad510a93588f6e9352279
elbatouri rabi <elbatouri@gmail.com>
2023-11-18 14:55:13 -0800

    fliter cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" list all cities of a state"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
+    rows = cur.fetchall()
+    temp = list(row[0] for row in rows)
+    print(*temp, sep=", ")
+    cur.close()
+    conn.close()
