a66ffbfc206a20e6fdd713afbf86e8956353f890
elbatouri rabi <elbatouri@gmail.com>
2023-11-18 16:19:46 -0800

    fix table state primerkey

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,9 +8,11 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/delete-state.sql
+++ 0x0F-python-object_relational_mapping/delete-state.sql
@@ -2,4 +2,4 @@
 USE hbtn_0e_6_usa;
 
 -- Specify the state IDs you want to delete
-DELETE FROM states WHERE id IN (6, 8, 9, 10);
+DELETE FROM states WHERE id IN (1, 2, 3, 4, 5);
