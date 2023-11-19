0e48d9c39b46a5b4c6596af96589374b6d563472
elbatouri rabi <elbatouri@gmail.com>
2023-11-18 16:57:56 -0800

    13 task fixed

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).filter(State.name.like('%a%'))
-    for state in states):
+    for state in states:
        session.delete(state) 
     session.commit()
     session.close()
