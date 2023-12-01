6c3df023915ff93c94e18722d23859636a53b44d
elbatouri rabi <elbatouri@gmail.com>
2023-12-01 14:21:35 -0800

    fix task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,8 +10,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
-    data = {'email': email}
-    response = requests.post(url, data=data)
-
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
     print("Your email is: {}".format(response.text))
