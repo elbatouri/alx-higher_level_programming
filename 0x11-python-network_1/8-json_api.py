d4735d418e08897da39dceaf47feaa9c081f1791
elbatouri rabi <elbatouri@gmail.com>
2023-12-01 15:22:35 -0800

    fix tsk 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,22 +5,22 @@ Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
   - If no letter is provided, sends `q=""`.
 """
-
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
-    try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-        json_data = response.json()
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
 
-        if json_data:
-            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
+    try:
+        response = r.json()
+        if response:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
         else:
             print("No result")
-
     except ValueError:
         print("Not a valid JSON")
