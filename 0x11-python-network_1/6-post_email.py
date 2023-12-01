3e27a6208894c088fc19b388be1134791b61d91d
elbatouri rabi <elbatouri@gmail.com>
2023-12-01 14:26:00 -0800

    fix 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,9 +7,10 @@ and finally displays the body of the response.
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    payload = {'email': email}
-    response = requests.post(url, data=payload)
-    print("Your email is: {}".format(response.text))
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
