da755ba15d70ac33c470ccd1ea8f765ad3a8da25
elbatouri rabi <elbatouri@gmail.com>
2023-12-01 14:47:38 -0800

    fix task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,11 +3,12 @@
 Python script that sends a search request to the
 github API, searching people
 """
-import requests
 import sys
-from request.auth import HTTPBasicAuth
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    reques = requests.get("https://api.github.com/user", auth=auth)
-    print(reques.json().get("id"))
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
