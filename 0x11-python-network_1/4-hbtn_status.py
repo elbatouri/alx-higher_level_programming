d18541491f6dcf30ed3b345fcd34171b80cffa83
elbatouri rabi <elbatouri@gmail.com>
2023-12-01 14:43:46 -0800

    fiw task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """
 Python script that fetches https://alx-intranet.hbtn.io/status
 using the requests package and displays information about the response.
@@ -6,7 +6,7 @@ using the requests package and displays information about the response.
 import requests
 
 if __name__ == '__main__':
-    response = requests.get("https://alx-intranet.hbtn.io/status")
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
