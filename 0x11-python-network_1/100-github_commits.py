2bc246e62f54edc0700f2407ea1ac614bc8e3603
elbatouri rabi <elbatouri@gmail.com>
2023-12-01 15:26:34 -0800

    advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script to list 10 commits (from the most recent to oldest)
+of a given repository by a specified user using GitHub API.
+"""
+import sys
+import requests
+
+def get_commits(repo, owner):
+    """
+    Get the list of commits from the GitHub API
+    """
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Unable to fetch commits. Status code: {response.status_code}")
+        return None
+
+def print_commits(commit_list):
+    """
+    Print the commits, up to 10 commits
+    """
+    for i, commit in enumerate(commit_list[:10]):
+        sha = commit.get('sha')
+        author_name = commit.get('commit', {}).get('author', {}).get('name', 'Unknown Author')
+        print(f'{sha}: {author_name}')
+
+def main(argv):
+    """
+    Main function to list 10 commits for a given repository and owner
+    """
+    repo = argv[1]
+    owner = argv[2]
+
+    commit_list = get_commits(repo, owner)
+    if commit_list is not None:
+        print_commits(commit_list)
+
+if __name__ == "__main__":
+    main(sys.argv)
