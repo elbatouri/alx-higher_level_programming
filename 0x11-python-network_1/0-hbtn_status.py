b6b372a3e8afaaa9cbb884724c6e6ec03d836f51
elbatouri rabi <elbatouri@gmail.com>
2023-12-01 13:33:11 -0800

    update tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-this script will:
-    - fetches https://alx-intranet.hbtn.io/status
-    - use the package urllib
-    - use a with statement
+This script fetches https://alx-intranet.hbtn.io/status
+using the urllib package and displays information about the response.
 """
+
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body_content = response.read()
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body_content)))
-        print("\t- content: {}".format(body_content))
-        print("\t- utf8 content: {}".format(body_content.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
