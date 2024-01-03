1c45ab726b741b336e9ff5fd9ff0809c1c1a1cbf
elbatouri rabi <elbatouri@gmail.com>
2024-01-03 13:10:11 -0800

    task 2 done

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs')
+
+const filePath = process.argv[2]
+const contentToWrite = process.argv[3]
+
+fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+	if (error) {
+		console.error(error);
+	} else {
+		console.log("ok");
+	}
+});
