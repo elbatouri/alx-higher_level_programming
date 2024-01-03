83cb8622a16733cd87ea4d5e4dcbb6c0f1bfb6ac
elbatouri rabi <elbatouri@gmail.com>
2024-01-03 13:21:40 -0800

    fix the semistandar bugs

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
-const filePath = process.argv[2]
-const contentToWrite = process.argv[3]
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
 
 fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
-	if (error) {
-		console.log(error);
-	}
+  if (error) {
+    console.log(error);
+  }
 });
