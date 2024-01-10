e50224c38bbf1add901964b05573faf3b21b128a
el batouri rabi <elbatouri@gmail.com>
2024-01-10 15:38:14 -0800

    semistandard final check

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,6 +1,6 @@
 // Wait for the document to be fully loaded
 $(document).ready(function () {
-    $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
-        $('#hello').text(data.hello);
-    });
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    $('#hello').text(data.hello);
+  });
 });
