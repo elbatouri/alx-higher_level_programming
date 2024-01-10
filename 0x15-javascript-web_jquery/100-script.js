9852d9907050a1c3b0af0205ceaa8462f0efb379
el batouri rabi <elbatouri@gmail.com>
2024-01-10 15:19:26 -0800

    task 10

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Select the <header> element and update its text color to red
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('HEADER').style.color = '#FF0000';
+})
